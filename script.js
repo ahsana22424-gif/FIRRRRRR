13b264dd8c602b2466d3df17b6635be21a35f223
ahsana22424-gif <ahsana22424@gmail.com>
2025-10-28 11:35:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. pilih tombol berdasarkan ID-nya
+const tombo1sapa =docomen .get1ementById('sapaButton')
+//2. tambahkan 'event listener'untuk aksi 'clik'
+tombo1sapa,addeventlistener ('click ',function() {
+     //3.jalan kode ini ketika tombol di-klic
+    alert('halo!terima kasik telah berkunjung!');
+  )};
